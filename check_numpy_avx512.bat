49a00262f4dae5501a839cf40870fcd3b349b128
swap357 <swapnilpatel357@gmail.com>
2025-11-14 01:25:37 +0530

    Enhance readability of NumPy AVX512_SKX detection command in batch script

--- check_numpy_avx512.bat
+++ check_numpy_avx512.bat
@@ -2,7 +2,11 @@
 @rem reported to support the feature and NumPy >= 1.22 as this results in the use
 @rem of low accuracy SVML libm replacements in ufunc loops.
 
-for /f %%i in ('python -c "from numba.misc import numba_sysinfo; sysinfo=numba_sysinfo.get_sysinfo(); print(sysinfo[\"NumPy AVX512_SKX detected\"] and sysinfo[\"NumPy Version\"]>=\"1.22\")"') do set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
+for /f %%i in (^
+'python -c "from numba.misc import numba_sysinfo; ^
+sysinfo=numba_sysinfo.get_sysinfo(); ^
+print(sysinfo[\"NumPy AVX512_SKX detected\"] and sysinfo[\"NumPy Version\"]>=\"1.22\")"'^
+) do set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
 
 echo NumPy ^>= 1.22 with AVX512_SKX detected: %NUMPY_DETECTS_AVX512_SKX_NP_GT_122%
 
