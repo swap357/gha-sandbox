b529b189e6dd06d422fe6ea9c50ff892786eeb96
swap357 <swapnilpatel357@gmail.com>
2025-11-14 01:21:56 +0530

    Refactor NumPy AVX512_SKX detection command in batch script for improved readability

--- check_numpy_avx512.bat
+++ check_numpy_avx512.bat
@@ -1,11 +1,8 @@
 @rem Disable NumPy dispatching to AVX512_SKX feature extensions if the chip is
 @rem reported to support the feature and NumPy >= 1.22 as this results in the use
 @rem of low accuracy SVML libm replacements in ufunc loops.
-set _NPY_CMD=from numba.misc import numba_sysinfo;^
-sysinfo=numba_sysinfo.get_sysinfo();^
-print(sysinfo['NumPy AVX512_SKX detected'] and sysinfo['NumPy Version']^>='1.22')
 
-for /f %%i in ('python -c %_NPY_CMD%') do set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
+for /f %%i in ('python -c "from numba.misc import numba_sysinfo; sysinfo=numba_sysinfo.get_sysinfo(); print(sysinfo[\"NumPy AVX512_SKX detected\"] and sysinfo[\"NumPy Version\"]>=\"1.22\")"') do set NUMPY_DETECTS_AVX512_SKX_NP_GT_122=%%i
 
 echo NumPy ^>= 1.22 with AVX512_SKX detected: %NUMPY_DETECTS_AVX512_SKX_NP_GT_122%
 
